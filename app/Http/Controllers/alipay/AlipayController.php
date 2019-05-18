d9aacba47d26d80d918214e8fc9eb842ebbd7931
fengdan <2128516529@qq.com>
2019-05-18 18:15:41 +0800

    支付

--- app/Http/Controllers/alipay/AlipayController.php
+++ app/Http/Controllers/alipay/AlipayController.php
@@ -150,6 +150,7 @@ class AlipayController extends Controller
         $p = json_encode($_POST);
         $log_str = "\n>>>>>> " .date('Y-m-d H:i:s') . ' '.$p . " \n";
         file_put_contents('/logs/alipay_notify',$log_str,FILE_APPEND);
+        exit;
         //TODO 验签 更新订单状态
         $out_trade_no=$p['out_trade_no'];
         $where=[
